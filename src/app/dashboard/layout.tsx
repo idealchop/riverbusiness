94428291b915087bd3edefbc1011dbc54b4094f0
Jimboy Regalado <jimbs.work@gmail.com>
2025-11-30 16:07:37 +0000

    Try fixing this error: `Runtime Error: Error: useMemo is not defined. Er

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Image from 'next/image';
 import {
   DropdownMenu,
