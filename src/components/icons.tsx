6175e85170b73c4e5d9952827beb2b2ab24c741e
Joross Mangubat <jorossmangubat84@gmail.com>
2025-11-30 04:05:21 +0000

    Seems the logo is too big.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,16 +3,13 @@ import Image from 'next/image';
 
 export function Logo(props: SVGProps<SVGSVGElement> & { width?: number; height?: number }) {
   return (
-    <div style={{ width: '100%', height: 'auto', position: 'relative' }} className={props.className}>
-        <Image
-        src="https://firebasestorage.googleapis.com/v0/b/smartrefill-singapore/o/River%20Mobile%2FLogo%2FRiverAI_Icon_Blue_HQ.png?alt=media&token=2d84c0cb-3515-4c4c-b62d-2b61ef75c35c"
-        alt="River Business Logo"
-        width={props.width || 0}
-        height={props.height || 0}
-        sizes="100vw"
-        style={{ width: '100%', height: 'auto' }}
-        unoptimized
-        />
-    </div>
+    <Image
+      src="https://firebasestorage.googleapis.com/v0/b/smartrefill-singapore/o/River%20Mobile%2FLogo%2FRiverAI_Icon_Blue_HQ.png?alt=media&token=2d84c0cb-3515-4c4c-b62d-2b61ef75c35c"
+      alt="River Business Logo"
+      width={props.width || 0}
+      height={props.height || 0}
+      className={props.className}
+      unoptimized
+    />
   );
 }
