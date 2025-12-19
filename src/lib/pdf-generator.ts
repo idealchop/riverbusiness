e9fea31b02842dfdedcc9b0629e032afa3494883
Jimboy Regalado <jimbs.work@gmail.com>
2025-12-19 17:27:11 +0000

    this text in the header. River Philippines
    Turn Everyday Needs Intro Au

--- src/lib/pdf-generator.ts
+++ src/lib/pdf-generator.ts
@@ -36,7 +36,11 @@ export const generateSOA = (user: AppUser, deliveries: Delivery[], dateRange?: D
   doc.setFont('helvetica', 'bold');
   doc.setTextColor(255, 255, 255);
   doc.text('Water Refill History', 36, 17);
-  doc.text('River Philippines', pageWidth - 14, 17, { align: 'right' });
+
+  doc.setFontSize(10);
+  doc.text('River Philippines', pageWidth - 14, 12, { align: 'right' });
+  doc.setFontSize(8);
+  doc.text('Turn Everyday Needs Into Automatic Experience With River', pageWidth - 14, 18, { align: 'right' });
 
 
   // 2. Add Personalized Greeting and Info
