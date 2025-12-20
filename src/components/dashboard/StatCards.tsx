e21c80740b6fc099cf53951a25864ee5b2094767
Jimboy Regalado <jimbs.work@gmail.com>
2025-12-20 10:02:45 +0000

    In mobile view this will be 2x2 (_for element <StatCards>_)

--- src/components/dashboard/StatCards.tsx
+++ src/components/dashboard/StatCards.tsx
@@ -137,7 +137,7 @@ export function StatCards({
   const isFlowPlan = user?.plan?.isConsumptionBased;
 
   return (
-    <div className="grid grid-cols-1 gap-4 md:gap-6 lg:grid-cols-4">
+    <div className="grid grid-cols-2 gap-4 md:gap-6 lg:grid-cols-4">
       {isFlowPlan ? (
         <div className="col-span-full grid grid-cols-1 lg:grid-cols-3 gap-6">
           <Card className="flex flex-col lg:col-span-2">
