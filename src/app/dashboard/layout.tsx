af7fc2ebe7d43bb15114dc6a9ff29e92af4cf68b
Jimboy Regalado <jimbs.work@gmail.com>
2025-12-20 04:06:45 +0000

    is it really connected now?

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -446,7 +446,7 @@ export default function DashboardLayout({
                 {hasNewMessage && <span className="absolute -top-1 -right-1 h-3 w-3 rounded-full bg-red-500 border-2 border-background" />}
               </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-4xl h-full sm:h-[95vh] flex flex-col sm:rounded-lg">
+            <DialogContent className="sm:max-w-4xl max-h-[95vh] flex flex-col sm:rounded-lg">
                 <DialogHeader>
                     <DialogTitle className="text-3xl font-bold">Hello, {user?.businessName}!</DialogTitle>
                     <DialogDescription>
