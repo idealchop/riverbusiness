c676bc110728ad8ccb6c24293b5fc2b75e3f4ca6
Jimboy Regalado <jimbs.work@gmail.com>
2025-12-20 03:26:36 +0000

    make the max heigh 90% vh here and the napply scroll function. (_for ele

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -447,7 +447,7 @@ export default function DashboardLayout({
                 {hasNewMessage && <span className="absolute -top-1 -right-1 h-3 w-3 rounded-full bg-red-500 border-2 border-background" />}
               </Button>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-4xl h-full w-full rounded-none border-none sm:rounded-lg sm:max-h-[85vh] flex flex-col">
+            <DialogContent className="sm:max-w-4xl h-full w-full rounded-none border-none sm:rounded-lg max-h-[90vh] flex flex-col">
                 <DialogHeader>
                     <DialogTitle className="text-3xl font-bold">Hello, {user?.businessName}!</DialogTitle>
                     <DialogDescription>
