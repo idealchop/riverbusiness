d468dcc012d1ea1168efb36ad0edfe693baf0f00
Jimboy Regalado <jimbs.work@gmail.com>
2026-01-03 04:59:49 +0000

    Try fixing this error: `Runtime ReferenceError: useMemo is not defined.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Bell, User, Receipt, FileUp, Info, MessageSquare } from 'lucide-react';
