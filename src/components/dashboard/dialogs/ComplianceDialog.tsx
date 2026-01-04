7a7aa17210d3bc65661e89770cbbf478737d66e0
Jimboy Regalado <jimbs.work@gmail.com>
2026-01-04 13:16:39 +0000

    okay. Seems the view report is only good for mobile. Make sure it is goo

--- src/components/dashboard/dialogs/ComplianceDialog.tsx
+++ src/components/dashboard/dialogs/ComplianceDialog.tsx
@@ -290,7 +290,7 @@ export function ComplianceDialog({
                         </Card>
                     )}
                     
-                    <div className="space-y-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                         {selectedSanitationVisit?.dispenserReports?.map(report => (
                              <Card key={report.dispenserId}>
                                 <CardHeader>
