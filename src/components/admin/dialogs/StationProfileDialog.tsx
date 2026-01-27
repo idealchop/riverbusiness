bdd89bc2a3d3e2415adc336686af810052b94605
Jimboy Regalado <jimbs.work@gmail.com>
2026-01-27 14:34:33 +0000

    Try fixing this error: `Runtime ReferenceError: Label is not defined. Er

--- src/components/admin/dialogs/StationProfileDialog.tsx
+++ src/components/admin/dialogs/StationProfileDialog.tsx
@@ -8,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogFooter, DialogClose } from '@/components/ui/dialog';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
 import { useToast } from '@/hooks/use-toast';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
