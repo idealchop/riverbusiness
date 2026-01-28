251d8507539cd34ab1e45943104210bf23b53ead
Jimboy Regalado <jimbs.work@gmail.com>
2026-01-28 03:37:35 +0000

    Optimize Admin Dashboard

--- src/components/dashboard/layout.tsx
+++ src/components/dashboard/layout.tsx
@@ -110,8 +110,8 @@ export default function DashboardLayout({
 
   const paymentOptions: PaymentOption[] = useMemo(() => [
       { name: 'GCash', qr: gcashQr, details: { accountName: 'Jamie Camille Liongson', accountNumber: '09989811596' } },
-      { name: 'BPI', qr: bankQr, details: { accountName: 'Jamie Camille Liongson', accountNumber: '3489145013' } },
-      { name: 'PayMaya', qr: paymayaQr, details: { accountName: 'Jamie Camille Liongson', accountNumber: '09557750188' } },
+      { name: 'BPI', qr: bankQr, details: { accountName: 'Jimboy Regalado', accountNumber: '3489145013' } },
+      { name: 'PayMaya', qr: paymayaQr, details: { accountName: 'Jimboy Regalado', accountNumber: '09557750188' } },
       { name: 'Credit Card', qr: cardQr }
   ], [gcashQr, bankQr, paymayaQr, cardQr]);
   
