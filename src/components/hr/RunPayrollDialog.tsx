0eb961083dbc715653bce7fe3d6e38ba68dfd448
Jimboy Regalado <jimbs.work@gmail.com>
2026-05-08 13:52:31 +0000

    Try fixing this error: `Runtime ReferenceError: Label is not defined. Er

--- src/components/hr/RunPayrollDialog.tsx
+++ src/components/hr/RunPayrollDialog.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import React, { useState, useEffect } from 'react';
@@ -15,6 +14,7 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { 
   Form, 
   FormControl, 
